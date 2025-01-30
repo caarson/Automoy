f4abfa1ae500d6c1c307a5d0e5ab77f7fcdc5dc8
Carson Rhodes <imitremc@gmail.com>
2025-01-29 18:29:23 -0800

    Further integration and addition of YOLO model to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 # Read the contents of your requirements.txt file
 with open("requirements.txt") as f:
@@ -7,6 +8,10 @@ with open("requirements.txt") as f:
 # Read the contents of your README.md file for the project description
 with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
+
+# Define model path
+model_path = "operate/data/YOLO/models"
+model_file = os.path.join(model_path, "socYOLO.pt")
 
 setup(
     name="self-operating-computer",
@@ -19,9 +24,14 @@ setup(
         ],
     },
     package_data={
-        # Include the file in the operate.models.weights package
+        # Include model weights
         "operate.models.weights": ["best.pt"],
+        "operate.data.YOLO.models": ["socYOLO.pt"],  # Explicitly add socYOLO.pt
     },
+    data_files=[
+        (model_path, [model_file])  # Ensure the file is copied to the right location
+    ],
+    include_package_data=True,  # Ensures non-code files are included
     long_description=long_description,  # Add project description here
     long_description_content_type="text/markdown",  # Specify Markdown format
     # include any other necessary setup options here
