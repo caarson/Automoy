50ba549dba7351ccf75a6d8fa6f0b75f35dfd4bc
Carson Rhodes <imitremc@gmail.com>
2025-02-10 17:49:01 -0800

    Update prompts.py

--- operate/utils/prompts.py
+++ operate/utils/prompts.py
@@ -76,7 +76,7 @@ Coordinates: The item's X Y location on the screen.
 ✅ **You must use OCR and YOLO provided information to interact with the system.**
 
 ### **ADDITIONAL CONTEXTUAL INFORMATION**
-✅ **You are on ra {operating_system} operating system.**
+✅ **You are on a {operating_system} operating system.**
 ✅ **Anything you want to do, like searching on the internet, you must open the application itself before performing any application-specific operations!**
 
 Most importantly, your objective is: {objective}
